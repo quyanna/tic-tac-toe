96316914f0614d41974f03c8f85e18a3cfa70c1e
quyanna <quyannacampbell@gmail.com>
2025-10-20 11:40:27 -0700

    Create working utility functions for Game Board.
    Adds a method to get empty status of board locations, a method to
    add markers to the board at a given index, and a method to get
    the entire board.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,43 @@
 console.log("Hello, world!");
+
+const GameBoard = (function (rows, cols, initFill = null) {
+  // Create a 2D Board with given rows and columns, initialized to "0";
+  const board = Array.from({ length: rows }, () =>
+    Array.from({ length: cols }).fill(0)
+  );
+
+  //NOTE: GameBoard interface is 1-indexed instead of 0-indexed, we only use 0-indexing internally.
+  // Valid indexes must be from 1-rows or 1-cols.
+  const indexValid = (row, col) => {
+    if (row > rows || col > cols || row <= 0 || col <= 0) {
+      console.log(Error("GameBoard index out of bounds")); //TODO: Change error messaging later maybe
+      return false;
+    } else return true;
+  };
+  //NOTE: GameBoard interface is 1-indexed instead of 0-indexed, we only use 0-indexing internally.
+  const isEmptyAt = (row, col) => {
+    if (indexValid(row, col) && board[row - 1][col - 1] === initFill) {
+      console.log(board[row - 1][col - 1]);
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const getBoard = () => board;
+
+  const addToBoard = (row, col, marker) => {
+    if (indexValid(row, col)) {
+      board[row - 1][col - 1] = marker;
+    }
+  };
+
+  return { getBoard, addToBoard, isEmptyAt };
+})(3, 3, 0);
+
+console.log(GameBoard.getBoard());
+console.log(GameBoard.isEmptyAt(1, 1));
+GameBoard.addToBoard(1, 1, "X");
+console.log(GameBoard.getBoard());
+console.log(GameBoard.isEmptyAt(1, 1));
+console.log(GameBoard.addToBoard(9, 9));
